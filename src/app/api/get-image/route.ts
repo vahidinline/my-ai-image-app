781ba9764513eff96f3f670ee9123aca30d0c143
vahidinline <vahidinline@gmail.com>
2024-09-20 15:01:08 +0100

    dd

--- src/app/api/get-image/route.ts
+++ src/app/api/get-image/route.ts
@@ -6,9 +6,14 @@ const keyFilePath = path.join(process.cwd(), 'src/config/key.json');
 
 async function getAccessToken() {
   const auth = new GoogleAuth({
-    keyFile: keyFilePath,
-    scopes: 'https://www.googleapis.com/auth/cloud-platform',
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    },
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
+
   const client = await auth.getClient();
   const accessToken = await client.getAccessToken();
   return accessToken.token;
