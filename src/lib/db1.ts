88728b392168dbc57fa1db83784f6bed6ef83e6f
vahidinline <vahidinline@gmail.com>
2024-10-28 12:03:56 +0000

    dd4

--- src/lib/db1.ts
+++ src/lib/db1.ts
@@ -12,10 +12,10 @@ const connectToDB = async () => {
       useUnifiedTopology: true,
     };
 
-    await mongoose.connect(process.env.MONGODB_URI, options);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error('Error connecting to MongoDB:');
     throw error; // Re-throw the error for handling in the calling function
   }
 };
